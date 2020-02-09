d17c80c5c1e303241f32bdf9e2d218ccafde6906
J. Michael Burgess <jmichaelburgess@gmail.com>
2020-02-09 17:21:33 +0100

    added base GBM notification

--- grbfunk/notifications.py
+++ grbfunk/notifications.py
@@ -43,3 +43,28 @@ class Notification(object):
         
     
 
+class GBMNotification(Notification):
+    
+    def __init__(self,root, notify_type):
+        
+        
+        super(GBMNotification, self).__init__(instrument_name='GBM',root=root, notify_type=notify_type)
+        
+        
+    def action(self):
+        self._form_burst_name()
+        
+        pos2d = self._root.find('.//{*}Position2D')
+        ra = float(pos2d.find('.//{*}C1').text)
+        dec = float(pos2d.find('.//{*}C2').text)
+        radius = float(pos2d.find('.//{*}Error2Radius').text)
+        
+    def _form_burst_name(self):
+        tmp = self._root.find('.//{*}ISOTime').text
+        yy, mm, dd = re.match('^\d\d(\d\d)-(\d\d)-(\d\d)T\d\d:\d\d:\d\d\.\d\d$', tmp).groups()
+        
+        
+        self._burst_name = 'GRB{yy}{mm}{dd}xxx'
+        
+    
+    
