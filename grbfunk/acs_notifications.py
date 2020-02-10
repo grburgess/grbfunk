fcbfbecb8b4e81ff38efa55ac5d64314eaf36fe9
grburgess <jmichaelburgess@gmail.com>
2020-02-10 19:12:04 +0100

    starting to add ACS alerts

---
+++ grbfunk/acs_notifications.py
@@ -0,0 +1,8 @@
+import os
+import re
+import numpy as np
+import coloredlogs, logging
+from grbfunk.notifications import Notification
+import grbfunk.utils.log
+
+logger = logging.getLogger("grbfunk.notification.ACS")
