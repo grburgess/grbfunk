d42c6ebece738024e0877f75fa74c26c6e8e6bb9
grburgess <jmichaelburgess@gmail.com>
2020-02-10 19:44:32 +0100

    minimal additions

--- grbfunk/acs_notifications.py
+++ grbfunk/acs_notifications.py
@@ -6,3 +6,23 @@ from grbfunk.notifications import Notification
 import grbfunk.utils.log
 
 logger = logging.getLogger("grbfunk.notification.ACS")
+
+
+class ACSNotification(Notification):
+    def __init__(self, root, notify_type):
+        """
+        Generic ACS  alert providing basic 
+        functionality
+
+        :param root: the parsed xml file
+        :param notify_type: the type of notification (str)
+        :returns: 
+        :rtype: 
+
+        """
+
+        super(ACSNotification, self).__init__(
+            instrument_name="ACS", root=root, notify_type=notify_type
+        )
+
+        logger.debug("ACS notification is being created")
