67ecee55a9fad7d3b8cd15d1d95a2dc50a0e9bd0
grburgess <jmichaelburgess@gmail.com>
2020-02-11 15:37:37 +0100

    cleaned up and added testing

--- grbfunk/bot.py
+++ grbfunk/bot.py
@@ -1,44 +1,104 @@
 import telegram
-import yaml
+import os
+
+from grbfunk.utils.package_data import get_access_file
 
 
-# we do not want to try and load all the tokens if they aren't there
+import coloredlogs, logging
+import grbfunk.utils.log
 
-path = os.path.join(os.path.expanduser("~"), ".grbfunk", "access.yaml")
-
-with open(path) as f:
-    # The FullLoader parameter handles the conversion from YAML
-    # scalar values to Python the dictionary format
-    access = yaml.load(f, Loader=yaml.SafeLoader)
-
-token = access["token"]
-chat_id = access["chat_id"]
+logger = logging.getLogger("grbfunk.bot")
 
 
 
-class Bot(self):
+_DEBUG = False
+if os.environ.get("GRBFUNK_DEBUG") is not None:
 
-    def __init__(self,name, token, chat_id):
+    if os.environ.get("GRBFUNK_DEBUG") == "True":
 
+        _DEBUG = True
+
+
+
+class Bot(object):
+    def __init__(self, name, token, chat_id):
+        """
+        A generic telegram bot
+
+        :param name: the name of the bot
+        :param token: the bot token
+        :param chat_id: the chat id to talk to
+        :returns: 
+        :rtype: 
+
+        """
+        logger.debug(f"{name} bot is being constructed")
+
+        # create the bot
         self._bot = telegram.Bot(token=token)
+
         self._name = name
+        self._chat_id = chat_id
+
+
+        
+        if _DEBUG:
+
+            # if we are testing we send stuff to a special
+            # chat. go ahead and spam me
+            
+            self._chat_id = '-1001284769525'
         
         self._msg_header = f"{self._name} says:\n"
 
-        
     def speak(self, message):
+        """
+        send a message
+
+        :param message: 
+        :returns: 
+        :rtype: 
+
+        """
 
         full_msg = f"{self._msg_header}{message}"
-        self._bot.send_message(chat_id = self._chat_id, text=full_msg)
+
+        logger.info(f"{self._name} bot is sending: {message}")
+
+        self._bot.send_message(chat_id=self._chat_id, text=full_msg)
 
     def show(self, image, description):
+        """
+        send an image
+
+        :param image: 
+        :param description: 
+        :returns: 
+        :rtype: 
+
+        """
 
         full_msg = f"{self._msg_header}{description}"
-        
-        
-        self._bot.send_photo(chat_id=self._chat_id,
-                             photo=open(image, "rb"), caption=full_msg)
 
-        
-        
-        
+        logger.info(f"{self._name} bot is sending the {description} image")
+
+        self._bot.send_photo(
+            chat_id=self._chat_id, photo=open(image, "rb"), caption=full_msg
+        )
+
+
+class GBMBot(Bot):
+    def __init__(self):
+        """
+        A GBM bot
+
+        :returns: 
+        :rtype: 
+
+        """
+
+        access = get_access_file()
+        token = access["token"]
+        chat_id = access["chat_id"]
+
+        super(GBMBot, self).__init__(name="GBM", token=token, chat_id=chat_id)
