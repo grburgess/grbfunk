8c4c930f50b1f8e69d124d263853ce818cabb2fb
grburgess <jmichaelburgess@gmail.com>
2020-02-12 18:21:03 +0100

    added bot to test

--- grbfunk/test/test_file_download.py
+++ grbfunk/test/test_file_download.py
@@ -5,6 +5,7 @@ import time
 import os
 from grbfunk.utils.download_file import BackgroundDownload
 from grbfunk.utils.process_counter import _global_proccess_counter
+from grbfunk.bot import Bot
 import coloredlogs, logging
 import grbfunk.utils.log
 
@@ -27,12 +28,32 @@ thread = threading.Thread(target=server)
 thread.daemon = True
 thread.start()
 
+_DEBUG = False
+_TESTING = False
+if os.environ.get("GRBFUNK_DEBUG") is not None:
+
+    if os.environ.get("GRBFUNK_DEBUG") == "True":
+
+        _DEBUG = True
+
+    # for travisCI
+    _TRAVIS_TOKEN = os.environ.get("TOKEN")
+    
+    if _TRAVIS_TOKEN is not None:
+
+        _TESTING = True
+
+
 
 def test_background_download():
 
     url = "http://0.0.0.0:8080/clowns.txt"
 
-    dl = BackgroundDownload(url, _global_proccess_counter, wait_time=1, max_time=60)
+    token = _TRAVIS_TOKEN
+    
+    bot = Bot('test',token = token, chat_id = None )
+    
+    dl = BackgroundDownload(url, _global_proccess_counter, bot=bot, wait_time=1, max_time=60)
 
     time.sleep(5)
 
@@ -47,6 +68,11 @@ def test_background_download_timeout():
 
     url = "http://0.0.0.0:8080/clowns.txt"
 
-    dl = BackgroundDownload(url, _global_proccess_counter, wait_time=1, max_time=9)
+    token = _TRAVIS_TOKEN
+    
+    bot = Bot('test',token = token, chat_id = None )
+
+    
+    dl = BackgroundDownload(url, _global_proccess_counter, bot=bot, wait_time=1, max_time=9)
 
     time.sleep(10)
